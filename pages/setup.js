def911f9f3a9ee62e250a5e0cccf5d5c88c0da5c
Joseph Boman <seph.hawkins@gmail.com>
2022-09-27 00:33:47 -0700

    Update Setup page to show faction tiles instead of being a basic dropdown.

--- pages/setup.js
+++ pages/setup.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
+import { FactionTile } from "../src/FactionCard";
+import { BasicFactionTile } from "../src/FactionTile";
+import { Modal } from "../src/Modal";
 
 const fetcher = async (url) => {
   const res = await fetch(url)
@@ -11,6 +14,21 @@ const fetcher = async (url) => {
   }
   return data
 };
+
+function getFactionColor(color) {
+  if (!color) {
+    return "#555";
+  }
+  switch (color) {
+    case "Blue":
+      return "cornflowerblue";
+    // case "Magenta":
+    //   return "hotpink";
+    // case "Green":
+    //   return "mediumseagreen";
+  }
+  return color;
+}
 
 function PlayerSelect({
   faction,
@@ -44,6 +62,14 @@ function PlayerSelect({
     }
     return true;
   });
+  const filteredColors = colors.filter((color) => {
+    if (color === "Magenta" || color === "Orange") {
+      if (!expansions.has("pok")) {
+        return false;
+      }
+    }
+    return true;
+  });
 
   function setFaction(event) {
     const value = event.target.value === "" ? null : event.target.value;
@@ -55,7 +81,7 @@ function PlayerSelect({
   }
   return (
     <div>
-      <label>Speaker</label>
+      <label>SPEAKER</label>
       <input
         name="speaker"
         type="checkbox"
@@ -65,6 +91,7 @@ function PlayerSelect({
       <select
         onChange={setFaction}
         value={faction.name === null ? "" : faction.name}
+        style={{fontFamily: 'Myriad Pro'}}
       >
         <option key="None" value="" disabled hidden>
           Select Faction
@@ -84,7 +111,7 @@ function PlayerSelect({
         <option key="None" value="">
           Select Color
         </option>
-        {colors.map((color) => {
+        {filteredColors.map((color) => {
           return (
             <option key={color} value={color} style={selectedColors.includes(color) ? { color: "grey" } : {}}>
               {color}
@@ -94,6 +121,116 @@ function PlayerSelect({
       </select>
     </div>
   );
+}
+
+function FactionSelect({ faction, isSpeaker, setFaction, setColor, setSpeaker, expansions, opts }) {
+  const [showFactionModal, setShowFactionModal] = useState(false);
+  const [showColorModal, setShowColorModal] = useState(false);
+  const { data: availableFactions, error: factionError } = useSWR("/api/factions", fetcher);
+  const { data: colors, error: colorError } = useSWR("/api/colors", fetcher);
+
+  if (factionError) {
+    return (<div>Failed to load factions</div>);
+  }
+  if (colorError) {
+    return (<div>Failed to load colors</div>);
+  }
+  if (!availableFactions || !colors) {
+    return (<div>Loading...</div>);
+  }
+
+  const filteredFactions = Object.entries(availableFactions).filter(([name, faction]) => {
+    if (faction.game === "base") {
+      return true;
+    }
+    if (!expansions.has(faction.game)) {
+      return false;
+    }
+    return true;
+  });
+  const filteredColors = colors.filter((color) => {
+    if (color === "Magenta" || color === "Orange") {
+      if (!expansions.has("pok")) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  function selectFaction(factionName) {
+    setShowFactionModal(false);
+    setFaction(factionName);
+  }
+  
+  function selectColor(color) {
+    setShowColorModal(false);
+    setColor(color);
+  }
+
+  const menuButtons = [];
+  if (!faction.name) {
+    menuButtons.push({
+      text: "Select Faction",
+      action: () => setShowFactionModal(true),
+    });
+  } else {
+    menuButtons.push({
+      text: "Change Faction",
+      action: () => setShowFactionModal(true),
+    });
+  }
+  if (!faction.color) {
+    menuButtons.push({
+      text: "Select Color",
+      action: () => setShowColorModal(true),
+    });
+  } else {
+    menuButtons.push({
+      text: "Change Color",
+      action: () => setShowColorModal(true),
+    });
+  }
+  if (!isSpeaker) {
+    menuButtons.push({
+      text: "Make Speaker",
+      action: () => setSpeaker(),
+    });
+  }
+
+  const color = getFactionColor(faction.color);
+
+  return (
+    <div style={{flexBasis: "50%"}}>
+      <Modal top="20%" closeMenu={() => setShowFactionModal(false)} visible={showFactionModal} title="Select Faction" content={
+        <div className="flexRow" style={{padding: "16px", flexWrap: "wrap", gap: "16px 40px"}}>
+          {filteredFactions.map(([factionName, faction]) => {
+            faction.color = color;
+            return (
+              <div style={{flexBasis: "15%", flexGrow: 2, flexShrink: 2}}>
+                <BasicFactionTile faction={faction} onClick={() => selectFaction(faction.name)} />
+              </div>
+            );
+          })}
+        </div>
+      } />
+      <Modal top="20%" closeMenu={() => setShowColorModal(false)} visible={showColorModal} title="Select Color" content={
+        <div className="flexRow" style={{padding: "16px", flexWrap: "wrap", gap: "16px 40px"}}>
+          {filteredColors.map((color) => {
+            const tempFaction = {
+              name: faction.name ?? null,
+              color: color,
+            };
+            return (
+              <div style={{flexBasis: "20%"}}>
+                <BasicFactionTile faction={tempFaction} onClick={() => selectColor(color)} />
+              </div>
+            );
+          })}
+        </div>
+      } />
+      <BasicFactionTile faction={faction} speaker={isSpeaker} menuButtons={menuButtons} opts={opts} />
+    </div>
+  )
 }
 
 const INITIAL_FACTIONS = [
@@ -133,7 +270,7 @@ const INITIAL_OPTIONS = {
 }
 
 export default function SetupPage() {
-  const [speaker, setSpeaker] = useState(0);
+  const [speaker, setSpeaker] = useState(-1);
   const [factions, setFactions] = useState(INITIAL_FACTIONS);
   const [options, setOptions] = useState(INITIAL_OPTIONS);
 
@@ -154,7 +291,7 @@ export default function SetupPage() {
 
   function reset() {
     setFactions(INITIAL_FACTIONS);
-    setSpeaker(0);
+    setSpeaker(-1);
   }
 
   function updatePlayerCount(count) {
@@ -181,6 +318,9 @@ export default function SetupPage() {
         }
       }
       setFactions(factions.slice(0, count));
+    }
+    if (speaker >= count) {
+      setSpeaker(0);
     }
   }
 
@@ -254,17 +394,97 @@ export default function SetupPage() {
     );
   }
 
+  function randomColors() {
+    let selectedColors = [];
+    for (let index = 0; index < factions.length; index++) {
+      if (factions[index].color !== null) {
+        selectedColors[index] = factions[index].color;
+      }
+    }
+    const filteredColors = colors.filter((color) => {
+      if (color === "Magenta" || color === "Orange") {
+        if (!options.expansions.has("pok")) {
+          return false;
+        }
+      }
+      return true;
+    });
+    for (let index = 0; index < factions.length; index++) {
+      if (factions[index].color !== null) {
+        continue;
+      }
+      let selectedColor = null;
+      while (
+        selectedColor === null ||
+        selectedColors.includes(selectedColor)
+      ) {
+        let randomIndex = Math.floor(Math.random() * filteredColors.length);
+        selectedColor = filteredColors[randomIndex];
+      }
+      selectedColors[index] = selectedColor;
+    }
+    setFactions(
+      factions.map((faction, index) => {
+        return { ...faction, color: selectedColors[index] };
+      })
+    );
+  }
+
   async function startGame() {
     const optionsToSend = {};
     optionsToSend.expansions = Array.from(options.expansions);
+    
+    // TODO: Consider just leaving gaps in the factions array to avoid this nonsense.
+    const factionsToSend = [];
+    let speakerToSend = 0;
+    factionsToSend.push(factions[0]);
+    if (factions.length > 6) {
+      factionsToSend.push(factions[6]);
+      if (speaker === 6) {
+        speakerToSend = factionsToSend.length - 1;
+      }
+    }
+    factionsToSend.push(factions[2]);
+    if (speaker === 2) {
+      speakerToSend = factionsToSend.length - 1;
+    }
+    if (factions.length > 5) {
+      factionsToSend.push(factions[5]);
+      if (speaker === 5) {
+        speakerToSend = factionsToSend.length - 1;
+      }
+    }
+    if (factions.length > 7) {
+      factionsToSend.push(factions[7]);
+      if (speaker === 7) {
+        speakerToSend = factionsToSend.length - 1;
+      }
+    }
+    if (factions.length > 3) {
+      factionsToSend.push(factions[3]);
+      if (speaker === 3) {
+        speakerToSend = factionsToSend.length - 1;
+      }
+    }
+    if (factions.length > 4) {
+      factionsToSend.push(factions[4]);
+      if (speaker === 4) {
+        speakerToSend = factionsToSend.length - 1;
+      }
+    }
+    factionsToSend.push(factions[1]);
+    if (speaker === 1) {
+      speakerToSend = factionsToSend.length - 1;
+    }
+
     const res = await fetch("/api/create-game", {
       method: "POST",
       headers: {
         'Content-Type': "application/json",
       },
       body: JSON.stringify({
-        factions: factions,
-        speaker: speaker,
+        factions: factionsToSend,
+        speaker: speakerToSend,
         options: optionsToSend,
       }),
     });
@@ -281,7 +501,19 @@ export default function SetupPage() {
     return true;
   }
 
+  function disableRandomizeColorsButton() {
+    for (let faction of factions) {
+      if (faction.color === null) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   function disableNextButton() {
+    if (speaker === -1) {
+      return true;
+    }
     for (let faction of factions) {
       if (faction.color === null || faction.name === null) {
         return true;
@@ -296,23 +528,24 @@ export default function SetupPage() {
       currentOptions.expansions.add(expansion);
     } else {
       currentOptions.expansions.delete(expansion);
+      setFactions(factions.map((faction, index) => {
+        const tempFaction = {...faction};
+        if (tempFaction.color === "Magenta" || tempFaction.color === "Orange") {
+          tempFaction.color = null;
+        }
+        if (!tempFaction.name || availableFactions[tempFaction.name].game === "base") {
+          return tempFaction;
+        }
+        if (!currentOptions.expansions.has(availableFactions[tempFaction.name].game)) {
+          tempFaction.name = null;
+        };
+        return tempFaction;
+      }));
       if (!currentOptions.expansions.has("pok")) {
         if (factions.length > 6) {
           updatePlayerCount(6);
         }
       }
-      setFactions(factions.map((faction, index) => {
-        if (!faction.name || availableFactions[faction.name].game === "base") {
-          return faction;
-        }
-        if (!currentOptions.expansions.has(availableFactions[faction.name].game)) {
-          return {
-            ...faction,
-            name: null
-          };
-        };
-        return faction;
-      }));
     }
     setOptions(currentOptions);
   }
@@ -323,19 +556,101 @@ export default function SetupPage() {
   const maxFactions = options.expansions.has("pok") ? 8 : 6;
 
   return (
-    <div className="App">
-      <label>Player Count</label>
+    <div className="flexColumn" style={{gap: "20px"}}>
       <div>
-        {[...Array(maxFactions - 2)].map((e, index) => {
-          const number = index + 3;
-          return (
-            <span key={number}>
-              <input onChange={(event) => updatePlayerCount(event.target.value)} type="radio" value={number} name="numPlayers" checked={factions.length === number} /> {number}
-            </span>
-          );
-        })}
+        <label>Player Count</label>
+        <div>
+          {[...Array(maxFactions - 2)].map((e, index) => {
+            const number = index + 3;
+            return (
+              <span key={number}>
+                <input onChange={(event) => updatePlayerCount(event.target.value)} type="radio" value={number} name="numPlayers" checked={factions.length === number} /> {number}
+              </span>
+            );
+          })}
+        </div>
       </div>
-      <div>
+      <div className="flexColumn" style={{position: "relative", gap: "120px", alignItems: "center"}}>
+        <div className="flexRow" style={{position: "absolute", width: "100%", height: "100%"}}>
+          TABLE (replace with image)
+        </div>
+        {/* Player 1 and 8 */}
+        <div className="flexRow" style={{gap: "60px"}}>
+          <FactionSelect faction={factions[0]}
+            isSpeaker={0 === speaker} 
+            setFaction={(value) => updatePlayerFaction(0, value)}
+            setColor={(value) => updatePlayerColor(0, value)}
+            setSpeaker={() => setSpeaker(0)}
+            expansions={options.expansions}
+            opts={{menuSide: "bottom"}} />
+          {factions.length > 6 ?
+            <FactionSelect faction={factions[6]}
+              isSpeaker={6 === speaker} 
+              setFaction={(value) => updatePlayerFaction(6, value)}
+              setColor={(value) => updatePlayerColor(6, value)}
+              setSpeaker={() => setSpeaker(6)}
+              expansions={options.expansions}
+              opts={{menuSide: "bottom"}} /> : null}
+        </div>
+        <div className="flexRow" style={{gap: "400px"}}>
+          {/* Player 2 and 5 */}
+          <div className="flexColumn" style={{gap: "80px"}}>
+            <FactionSelect faction={factions[1]}
+              isSpeaker={1 === speaker} 
+              setFaction={(value) => updatePlayerFaction(1, value)}
+              setColor={(value) => updatePlayerColor(1, value)}
+              setSpeaker={() => setSpeaker(1)}
+              expansions={options.expansions}
+              opts={{menuSide: "right"}} />
+            {factions.length > 4 ?
+              <FactionSelect faction={factions[4]}
+                isSpeaker={4 === speaker} 
+                setFaction={(value) => updatePlayerFaction(4, value)}
+                setColor={(value) => updatePlayerColor(4, value)}
+                setSpeaker={() => setSpeaker(4)}
+                expansions={options.expansions}
+                opts={{menuSide: "right"}} /> : null}
+          </div>
+          {/* Player 3 and 6 */}
+          <div className="flexColumn" style={{flexBasis: "10%", gap: "80px"}}>
+            <FactionSelect faction={factions[2]}
+              isSpeaker={2 === speaker} 
+              setFaction={(value) => updatePlayerFaction(2, value)}
+              setColor={(value) => updatePlayerColor(2, value)}
+              setSpeaker={() => setSpeaker(2)}
+              expansions={options.expansions}
+              opts={{menuSide: "left"}} />
+            {factions.length > 5 ?
+              <FactionSelect faction={factions[5]}
+                isSpeaker={5 === speaker} 
+                setFaction={(value) => updatePlayerFaction(5, value)}
+                setColor={(value) => updatePlayerColor(5, value)}
+                setSpeaker={() => setSpeaker(5)}
+                expansions={options.expansions}
+                opts={{menuSide: "left"}} /> : null}
+          </div>
+        </div>
+        {/* Player 4 and 8 */}
+        <div className="flexRow" style={{gap: "60px"}}>
+          {factions.length > 3 ?
+            <FactionSelect faction={factions[3]}
+              isSpeaker={3 === speaker} 
+              setFaction={(value) => updatePlayerFaction(3, value)}
+              setColor={(value) => updatePlayerColor(3, value)}
+              setSpeaker={() => setSpeaker(3)}
+              expansions={options.expansions}
+              opts={{menuSide: "top"}} /> : null}
+          {factions.length > 7 ?
+            <FactionSelect faction={factions[7]}
+              isSpeaker={7 === speaker} 
+              setFaction={(value) => updatePlayerFaction(7, value)}
+              setColor={(value) => updatePlayerColor(7, value)}
+              setSpeaker={() => setSpeaker(7)}
+              expansions={options.expansions}
+              opts={{menuSide: "top"}} /> : null}
+        </div>
+      </div>
+      {/* <div>
         {factions.map((faction, index) => {
           return (
             <PlayerSelect
@@ -351,7 +666,7 @@ export default function SetupPage() {
             />
           );
         })}
-      </div>
+      </div> */}
       <div>
         Options:
         <div>
@@ -392,17 +707,24 @@ export default function SetupPage() {
           </div>
         </div>
       </div>
-      <button onClick={reset}>Reset</button>
-      <button onClick={randomSpeaker}>Randomize Speaker</button>
-      <button
-        onClick={randomFactions}
-        disabled={disableRandomizeFactionButton()}
-      >
-        Randomize Remaining Factions
-      </button>
-      <button onClick={startGame} disabled={disableNextButton()}>
-        Start Game
-      </button>
+      <div className="flexColumn" style={{gap: "8px"}}>
+        <button onClick={reset}>Reset</button>
+        <div className="flexRow" style={{gap: "8px"}}>
+          <button onClick={randomSpeaker}>Randomize Speaker</button>
+          <button
+            onClick={randomFactions}
+            disabled={disableRandomizeFactionButton()}
+          >
+            Randomize Remaining Factions
+          </button>
+          <button onClick={randomColors}
+          disabled={disableRandomizeColorsButton()}
+          > Randomize Remaining Colors</button>
+        </div>
+        <button onClick={startGame} disabled={disableNextButton()}>
+          Start Game
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/FactionTile.js
@@ -0,0 +1,133 @@
+import { FactionSymbol } from "./FactionCard";
+import { useState } from "react";
+
+function getFactionColor(color) {
+  if (!color) {
+    return "#555";
+  }
+  switch (color) {
+    case "Blue":
+      return "cornflowerblue";
+    // case "Magenta":
+    //   return "hotpink";
+    // case "Green":
+    //   return "mediumseagreen";
+  }
+  return color;
+}
+
+/**
+ *
+ */
+export function BasicFactionTile({ faction, onClick, speaker, menuButtons, opts = {} }) {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function toggleMenu() {
+    setShowMenu(!showMenu);
+  }
+
+  function hideMenu() {
+    setShowMenu(false);
+  }
+
+  const iconStyle = {
+    width: "40px",
+    height: "40px",
+    position: "absolute",
+    zIndex: -1,
+    left: 0,
+    width: "100%",
+    opacity: "60%",
+  };
+
+  const color = getFactionColor(faction.color);
+  const name = faction.name ?? "Select Faction";
+  const border = "3px solid " + (faction.passed ? "#555" : color);
+
+  const menuButtonStyle = {
+    fontFamily: "Myriad Pro",
+    whiteSpace: "nowrap",
+    position: "absolute",
+    display: showMenu ? "flex" : "none",
+    zIndex: 2
+  };
+  switch (opts.menuSide) {
+    case "top":
+      menuButtonStyle.bottom = "100%";
+      menuButtonStyle.marginBottom = "4px";
+      menuButtonStyle.justifyContent = "flex-start";
+      menuButtonStyle.width = "100%";
+      break;
+    case "bottom":
+      menuButtonStyle.top = "100%";
+      menuButtonStyle.marginTop = "4px";
+      menuButtonStyle.justifyContent = "flex-start";
+      menuButtonStyle.width = "100%";
+      break;
+    case "left":
+      menuButtonStyle.right = "100%";
+      menuButtonStyle.marginRight = "4px";
+      menuButtonStyle.height = "40px";
+      menuButtonStyle.top = "0";
+      break;
+    case "right":
+    default:
+      menuButtonStyle.left = "100%";
+      menuButtonStyle.marginLeft = "4px";
+      menuButtonStyle.height = "40px";
+      menuButtonStyle.top = "0";
+      break;
+  }
+
+  console.log(opts);
+  return (
+    <div
+      style={{position: "relative"}}
+      tabIndex={0}
+      onBlur={onClick ? () => {} : hideMenu}>
+      <div
+        onClick={onClick ? onClick : toggleMenu}
+        style={{
+          borderRadius: "5px",
+          display: "flex",
+          flexDirection: "column",
+          border: border,
+          fontSize: opts.fontSize ?? "24px",
+          position: "relative",
+          cursor: (onClick || menuButtons.length !== 0) ? "pointer" : "auto",
+          alignItems: "center",
+          whiteSpace: "nowrap",
+          padding: "0px 4px",
+        }}
+      >
+        <div className="flexRow" style={{justifyContent: "flex-start", gap: "4px", padding: "0px 4px", height: "40px"}}>
+          {faction.name ? <div className="flexRow" style={iconStyle}>
+            <FactionSymbol faction={name} size={40} />
+          </div> : null}
+          {speaker ? <div style={{fontFamily: "Myriad Pro",
+            position: "absolute",
+            color: color === "Black" ? "#eee" : color,
+            borderRadius: "5px",
+            border: `2px solid ${color}`,
+            padding: "0px 2px",
+            fontSize: "12px",
+            top: "-10px",
+            left: "4px",
+            zIndex: 1,
+            backgroundColor: "#222"}}>
+            Speaker
+          </div> : null}
+          {opts.hideName ? null : <div style={{ textAlign: "center", position: "relative"}}>{name}</div>}
+        </div>
+      </div>
+      {menuButtons && menuButtons.length !== 0 ? <div className="flexColumn" style={menuButtonStyle}>
+        <div className="flexColumn" style={{alignItems: "stretch", gap: "4px"}}>
+          {menuButtons.map((button) => {
+            return (
+              <div key={button.text} style={{cursor: "pointer", gap: "4px", padding: "4px 8px", boxShadow: "1px 1px 4px black", backgroundColor: "#222", border: `2px solid ${color}`, borderRadius: "5px", fontSize: opts.fontSize ?? "16px"}} onClick={() => {hideMenu(); button.action()}}>{button.text}</div>)
+          })}
+        </div>
+      </div> : null}
+    </div>
+  );
+}
