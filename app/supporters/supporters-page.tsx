cffe6dafaf37b0ff2c46f7a16bd15ab7e10e8834
Joseph Boman <seph.hawkins@gmail.com>
2024-04-21 17:13:16 -0700

    Update supporters page to include section with people who have contributed to TI Assistant. (#31)

--- app/supporters/supporters-page.tsx
+++ app/supporters/supporters-page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import BorderedDiv from "../../src/components/BorderedDiv/BorderedDiv";
 import LabeledDiv from "../../src/components/LabeledDiv/LabeledDiv";
 import NonGameHeader from "../../src/components/NonGameHeader/NonGameHeader";
+import { FormattedMessage } from "react-intl";
 
 export default function Supporters() {
   return (
@@ -18,7 +19,40 @@ export default function Supporters() {
         }}
       >
         <LabeledDiv
-          label="Patrons"
+          label={
+            <FormattedMessage
+              id="355TBA"
+              defaultMessage="Contributors"
+              description="Label for section listing out people that worked on TI Assistant."
+            />
+          }
+        >
+          <div
+            className="flexColumn largeFont"
+            style={{
+              paddingBottom: "4px",
+              width: "100%",
+              fontSize: "24px",
+            }}
+          >
+            <div>
+              <FormattedMessage
+                id="kq4/fY"
+                defaultMessage="German Translation: {name}"
+                description="Credit for the german translation of TI Assistant."
+                values={{ name: "BuzZoiDk24" }}
+              />
+            </div>
+          </div>
+        </LabeledDiv>
+        <LabeledDiv
+          label={
+            <FormattedMessage
+              id="X3Nzbr"
+              defaultMessage="Patrons"
+              description="Label for section listing out people that regularly donate to TI Assistant."
+            />
+          }
           rightLabel={
             <a href={`https://patreon.com/TIAssistant`}>
               <div
@@ -27,7 +61,11 @@ export default function Supporters() {
                   width: "100%",
                 }}
               >
-                Become one here!
+                <FormattedMessage
+                  id="wOb6Wx"
+                  defaultMessage="Become a Patron"
+                  description="A button that will open the Patreon page."
+                />
               </div>
             </a>
           }
@@ -37,17 +75,17 @@ export default function Supporters() {
             style={{
               paddingBottom: "4px",
               width: "100%",
-              fontSize: "24px",
+              fontSize: "20px",
             }}
           >
             <div className="flexRow centered" style={{ width: "100%" }}>
-              Signoreliro
-            </div>
-            <div className="flexRow centered" style={{ width: "100%" }}>
-              Grant
+              Eric
             </div>
             <div className="flexRow centered" style={{ width: "100%" }}>
               Ilya
+            </div>
+            <div className="flexRow centered" style={{ width: "100%" }}>
+              Meanswell
             </div>
             <div className="flexRow centered" style={{ width: "100%" }}>
               Benjamin
@@ -55,7 +93,13 @@ export default function Supporters() {
           </div>
         </LabeledDiv>
         <LabeledDiv
-          label="Supporters"
+          label={
+            <FormattedMessage
+              id="dENKlQ"
+              defaultMessage="Supporters"
+              description="Label for section listing out everyone that has donated to TI Assistant."
+            />
+          }
           rightLabel={
             <a href={`https://www.buymeacoffee.com/tiassistant`}>
               <div
@@ -64,7 +108,11 @@ export default function Supporters() {
                   width: "100%",
                 }}
               >
-                Buy me a coffee here!
+                <FormattedMessage
+                  id="Alcp4i"
+                  defaultMessage="Buy me a coffee"
+                  description="A button that will open the Buy me a coffee page."
+                />
               </div>
             </a>
           }
@@ -74,14 +122,26 @@ export default function Supporters() {
             style={{
               paddingBottom: "4px",
               width: "100%",
-              fontSize: "24px",
+              fontSize: "20px",
             }}
           >
             <div className="flexRow centered" style={{ width: "100%" }}>
-              Worldly-Charity-9737
+              Grant
             </div>
             <div className="flexRow centered" style={{ width: "100%" }}>
               Ha
+            </div>
+            <div className="flexRow centered" style={{ width: "100%" }}>
+              Michael
+            </div>
+            <div className="flexRow centered" style={{ width: "100%" }}>
+              Signoreliro
+            </div>
+            <div className="flexRow centered" style={{ width: "100%" }}>
+              Will
+            </div>
+            <div className="flexRow centered" style={{ width: "100%" }}>
+              Worldly-Charity-9737
             </div>
           </div>
         </LabeledDiv>
