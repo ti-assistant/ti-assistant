02a07b6de5e334cd75f50aef212bd94daf002c52
Joseph Boman <seph.hawkins@gmail.com>
2024-11-22 16:30:05 -0800

    Fix issue where background processes slowed down site (#76)
    * Remove background processes.
    
    * Create API endpoint for processing games and cron job.

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -1,13 +1,6 @@
 import { Storage } from "@google-cloud/storage";
-import { createIntl, createIntlCache } from "react-intl";
 import "server-only";
-import { getBaseData } from "../../src/data/baseData";
-import { getLocale, getMessages } from "../../src/util/server";
-import {
-  maybeUpdateProcessedGames,
-  ProcessedGame,
-  reprocessGames,
-} from "../stats/processor";
+import { ProcessedGame } from "../stats/processor";
 import ArchivePage from "./archive-page";
 
 async function getJSONFileFromStorage(
@@ -22,19 +15,11 @@ async function getJSONFileFromStorage(
 }
 
 export default async function Page() {
-  const locale = getLocale();
-  const messages = await getMessages(locale);
-  const cache = createIntlCache();
-  const intl = createIntl({ locale, messages }, cache);
-
   const storage = new Storage({
     keyFilename: "./server/twilight-imperium-360307-ea7cce25efeb.json",
   });
 
-  const baseData = getBaseData(intl);
   const processedGames = await getJSONFileFromStorage(storage);
-
-  maybeUpdateProcessedGames(storage, processedGames, baseData);
 
   return <ArchivePage processedGames={processedGames} />;
 }
