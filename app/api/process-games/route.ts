03324648bb2bbd1dbfa8298128d34071b3b1c14a
Joseph Boman <seph.hawkins@gmail.com>
2024-11-23 14:36:56 -0800

    Fix issue with API endpoint.

--- app/api/process-games/route.ts
+++ app/api/process-games/route.ts
@@ -1,13 +1,8 @@
 import { NextResponse } from "next/server";
 import { rewriteProcessedGames } from "../../stats/processor";
 
-export async function GET(request: Request) {
-  const cronHeader = request.headers.get("x-appengine-cron");
-  if (!cronHeader) {
-    return NextResponse.error();
-  }
+export async function GET() {
+  await rewriteProcessedGames();
 
-  rewriteProcessedGames();
-
-  return;
+  return NextResponse.json({});
 }
