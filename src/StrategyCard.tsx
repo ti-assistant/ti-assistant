6f16b979b381d5b55ae958d0e4129ef5e5dae80d
Joseph Boman <seph.hawkins@gmail.com>
2024-12-01 16:20:36 -0800

    Fix Strategy Card sizing and move Promissory Menu to the lower left corner on Mobile.

--- src/StrategyCard.tsx
+++ src/StrategyCard.tsx
@@ -37,48 +37,49 @@ export function SmallStrategyCard({ cards }: SmallStrategyCardProps) {
       label={getFactionName(faction)}
       color={borderColor}
       style={{
-        display: "flex",
-        flexDirection: "column",
         height: height,
       }}
     >
-      <div
-        className="flexRow"
-        style={{
-          padding: `${rem(4)} ${rem(4)} ${rem(4)} 0`,
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
+      <div className="flexColumn" style={{ height: "100%", width: "100%" }}>
         <div
+          className="flexRow"
           style={{
-            flexBasis: "14%",
-            minWidth: rem(32),
-            fontSize: numFactions > 7 ? rem(28) : rem(32),
-            display: "flex",
-            justifyContent: "center",
-            color: faction?.passed ? "#555" : "#eee",
-            transition: "color 120ms",
+            padding: `${rem(4)} ${rem(4)} ${rem(4)} 0`,
+            justifyContent: "flex-start",
+            alignItems: "center",
+            width: "100%",
           }}
         >
-          {initiative}
-        </div>
-        <div
-          className="flexColumn"
-          style={{
-            flexBasis: "40%",
-            alignItems: "flex-start",
-            fontSize: numFactions > 7 ? rem(20) : rem(24),
-          }}
-        >
-          {cards.map((card) => {
-            const textColor = !card.used ? "#eee" : "#555";
-            return (
-              <div key={card.id} style={{ color: textColor }}>
-                {card.name}
-              </div>
-            );
-          })}
+          <div
+            style={{
+              flexBasis: "14%",
+              minWidth: rem(32),
+              fontSize: numFactions > 7 ? rem(28) : rem(32),
+              display: "flex",
+              justifyContent: "center",
+              color: faction?.passed ? "#555" : "#eee",
+              transition: "color 120ms",
+            }}
+          >
+            {initiative}
+          </div>
+          <div
+            className="flexColumn"
+            style={{
+              flexBasis: "40%",
+              alignItems: "flex-start",
+              fontSize: numFactions > 7 ? rem(20) : rem(24),
+            }}
+          >
+            {cards.map((card) => {
+              const textColor = !card.used ? "#eee" : "#555";
+              return (
+                <div key={card.id} style={{ color: textColor }}>
+                  {card.name}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </LabeledDiv>
