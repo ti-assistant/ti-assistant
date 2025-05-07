737a54bb417452a27334edb127e0cdaccea61f54
Joseph Boman <seph.hawkins@gmail.com>
2025-05-06 20:06:58 -0700

    Clean up SmallStrategyCard.

--- src/StrategyCard.tsx
+++ src/StrategyCard.tsx
@@ -1,5 +1,5 @@
 import LabeledDiv from "./components/LabeledDiv/LabeledDiv";
-import { useFactions } from "./context/factionDataHooks";
+import { useFaction, useNumFactions } from "./context/factionDataHooks";
 import { getFactionColor, getFactionName } from "./util/factions";
 import { rem } from "./util/util";
 
@@ -8,9 +8,11 @@ interface SmallStrategyCardProps {
 }
 
 export function SmallStrategyCard({ cards }: SmallStrategyCardProps) {
-  const factions = useFactions();
+  const numFactions = useNumFactions();
 
-  if (!cards[0]) {
+  const faction = useFaction(cards[0]?.faction ?? "Vuil'raith Cabal");
+
+  if (!faction) {
     return null;
   }
 
@@ -19,11 +21,6 @@ export function SmallStrategyCard({ cards }: SmallStrategyCardProps) {
     Number.MAX_SAFE_INTEGER
   );
 
-  const faction =
-    cards[0] && cards[0].faction ? factions[cards[0].faction] : undefined;
-
-  const numFactions = Object.keys(factions).length;
-
   let height = "auto";
   if (numFactions > 7) {
     height = rem(44);
@@ -31,7 +28,7 @@ export function SmallStrategyCard({ cards }: SmallStrategyCardProps) {
     height = rem(50);
   }
 
-  const borderColor = !faction?.passed ? getFactionColor(faction) : "#555";
+  const borderColor = !faction.passed ? getFactionColor(faction) : "#555";
   return (
     <LabeledDiv
       label={getFactionName(faction)}
@@ -40,46 +37,45 @@ export function SmallStrategyCard({ cards }: SmallStrategyCardProps) {
         height,
       }}
     >
-      <div className="flexColumn" style={{ height: "100%", width: "100%" }}>
+      <div
+        className="flexRow"
+        style={{
+          padding: `${rem(4)} ${rem(4)} ${rem(4)} 0`,
+          justifyContent: "flex-start",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+        }}
+      >
         <div
-          className="flexRow"
           style={{
-            padding: `${rem(4)} ${rem(4)} ${rem(4)} 0`,
-            justifyContent: "flex-start",
-            alignItems: "center",
-            width: "100%",
+            flexBasis: "14%",
+            minWidth: rem(32),
+            fontSize: numFactions > 7 ? rem(28) : rem(32),
+            display: "flex",
+            justifyContent: "center",
+            color: faction.passed ? "#555" : "#eee",
+            transition: "color 120ms",
           }}
         >
-          <div
-            style={{
-              flexBasis: "14%",
-              minWidth: rem(32),
-              fontSize: numFactions > 7 ? rem(28) : rem(32),
-              display: "flex",
-              justifyContent: "center",
-              color: faction?.passed ? "#555" : "#eee",
-              transition: "color 120ms",
-            }}
-          >
-            {initiative}
-          </div>
-          <div
-            className="flexColumn"
-            style={{
-              flexBasis: "40%",
-              alignItems: "flex-start",
-              fontSize: numFactions > 7 ? rem(20) : rem(24),
-            }}
-          >
-            {cards.map((card) => {
-              const textColor = !card.used ? "#eee" : "#555";
-              return (
-                <div key={card.id} style={{ color: textColor }}>
-                  {card.name}
-                </div>
-              );
-            })}
-          </div>
+          {initiative}
+        </div>
+        <div
+          className="flexColumn"
+          style={{
+            flexBasis: "40%",
+            alignItems: "flex-start",
+            fontSize: numFactions > 7 ? rem(20) : rem(24),
+          }}
+        >
+          {cards.map((card) => {
+            const textColor = !card.used ? "#eee" : "#555";
+            return (
+              <div key={card.id} style={{ color: textColor }}>
+                {card.name}
+              </div>
+            );
+          })}
         </div>
       </div>
     </LabeledDiv>
