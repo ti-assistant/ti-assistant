2a8e42d60807fadb349280b1b3ae8c05073f8897
Joseph Boman <seph.hawkins@gmail.com>
2025-09-13 21:22:49 -0700

    Allow 2 instances, and block off access to process-games.

--- app/api/process-games/route.ts
+++ app/api/process-games/route.ts
@@ -1,7 +1,14 @@
 import { NextResponse } from "next/server";
 import { rewriteProcessedGames } from "../../stats/processor";
+import { headers } from "next/headers";
 
 export async function GET() {
+  const headerList = headers();
+  const cron = headerList.get("X-Appengine-Cron");
+  if (!cron) {
+    throw new Error("Only cronjobs are allowed to call this endpoint.");
+  }
+
   await rewriteProcessedGames();
 
   return NextResponse.json({});
